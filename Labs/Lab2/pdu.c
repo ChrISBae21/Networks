874c04e80f4c5069bac2ab08b79cc9a4fb754b82
ChrISBae21 <cbgnote@gmail.com>
2024-04-09 18:50:46 -0700

    Finished part 3 of Lab 2

--- Labs/Lab2/pdu.c
+++ Labs/Lab2/pdu.c
@@ -15,4 +15,51 @@
 
 #include "networks.h"
 #include "safeUtil.h"
+#include "pdu.h"
 
+
+
+int sendPDU(int socketNumber, uint8_t * dataBuffer, int lengthOfData) {
+    uint8_t *pduBuffer;
+    int bytesSent;
+    uint16_t nLen, hLen;
+
+    hLen = lengthOfData + 2;
+    nLen = htons(lengthOfData + 2);
+    pduBuffer = (uint8_t*) sCalloc(hLen, sizeof(uint8_t));
+
+    memcpy(pduBuffer, &nLen, 2);
+    memcpy(pduBuffer+2, dataBuffer, lengthOfData);
+    bytesSent = safeSend(socketNumber, pduBuffer, hLen, 0);
+    
+    free(pduBuffer);
+    return bytesSent;
+}
+
+
+
+int recvPDU(int clientSocket, uint8_t * dataBuffer, int bufferSize) {
+    uint16_t pduHeader;         // Total PDU Length
+    uint16_t bytesReceived;
+
+    bytesReceived = safeRecv(clientSocket, (uint8_t*) &pduHeader, 2, MSG_WAITALL);
+
+    if(bytesReceived == 0) {
+        return 0;
+    }
+    
+    pduHeader = ntohs(pduHeader);       
+
+    if(bufferSize < pduHeader) {
+        perror("buffer size is too small on the receiving side\n");
+        exit(-1);
+    }
+
+    bytesReceived = safeRecv(clientSocket, dataBuffer, pduHeader, MSG_WAITALL);
+    if(bytesReceived == 0) {
+        return 0;
+    }
+    return bytesReceived;
+
+
+}
