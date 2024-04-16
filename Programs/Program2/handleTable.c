74978d8f6877891e154e8d1f63b268b71265e14f
ChrISBae21 <cbgnote@gmail.com>
2024-04-16 14:59:28 -0700

    advanced handle table

--- Programs/Program2/handleTable.c
+++ Programs/Program2/handleTable.c
@@ -20,14 +20,14 @@ void addHandle(uint8_t *handle, uint8_t handleLength, uint8_t socket);
 void setupHandleTable();
 void teardownHandleTable();
 void removeHandle(uint8_t *handle, uint8_t handleLength);
-
-
+uint8_t* getHandleName(uint8_t socketNo);
 
 // Handle Table Public Variables
 static HND_TBL *handleTable;
-static int maxFileDescriptor = 0;
-static int currentPollSetSize = 0;
-static int max = 10;
+static int max = 64;
+
+// static int maxFileDescriptor = 0;
+// static int currentPollSetSize = 0;
 
 typedef struct __attribute__((packed)) HND_TBL {
     uint8_t valid;          // Valid Handle
@@ -35,21 +35,18 @@ typedef struct __attribute__((packed)) HND_TBL {
 } HND_TBL;
 
 
+
+
 void setupHandleTable() {
     handleTable = (HND_TBL*) sCalloc(max, sizeof(HND_TBL));   
 }
 
-void addHandle(uint8_t *handle, uint8_t handleLength, uint8_t socket) {
+
+
+void addHandle(uint8_t *handle, uint8_t handleLength, uint8_t socketNo) {
     uint8_t flag = 1;
     HND_TBL *tempHandleTable = handleTable;
     uint8_t index = 0;
-
-
-
-    if(currentPollSetSize == max) {
-        max *= 2;
-        handleTable = srealloc(handleTable, max);
-    }
 
     while(flag) {
         if(!(tempHandleTable->valid)) {
@@ -61,8 +58,12 @@ void addHandle(uint8_t *handle, uint8_t handleLength, uint8_t socket) {
         index++;
         tempHandleTable += 1;       // next node
     }
-    
 
+
+    if(socketNo == max) {
+        max *= 2;
+        handleTable = srealloc(handleTable, max);
+    }
 }
 
 void removeHandle(uint8_t *handle, uint8_t handleLength) {
@@ -83,14 +84,49 @@ void teardownHandleTable() {
 
 
 // both these two functions need to check if the valid bit is set
-void getHandleName(uint8_t *handle, uint8_t handleLength) {
-    HND_TBL *tempHandleTable = handleTable;
 
-    while( (memcmp(tempHandleTable->handle, handle, handleLength) != 0) ) {
-        tempHandleTable += 1;
+/*
+* getHandleName
+* returns a pointer to the handle name if found
+* else returns NULL if it does not exist
+*/
+uint8_t* getHandleName(uint8_t socketNo) {
+    
+    if(handleTable[socketNo].valid) {
+        return &(handleTable[socketNo].handle);
     }
+
+    return NULL;
+    // HND_TBL *tempHandleTable = handleTable;
+
+    // while( (memcmp(tempHandleTable->handle, handle, handleLength) != 0) ) {
+    //     tempHandleTable += 1;
+    // }
 }
 
-void getSocketNumber() {
-    
+
+
+/*
+* getSocketNumber: returns the socket number for the handle name
+* if the handle doesn't exist, returns 0
+*/
+void getSocketNumber(uint8_t* handle, uint8_t len) {
+    HND_TBL *tempHandleTable = handleTable;
+    int flag = 1;
+    int socket = 0;
+
+
+
+
+    while(flag) {
+        if(!(tempHandleTable->valid)) {
+            tempHandleTable->valid = 1;
+            memcpy(tempHandleTable->handle, handle, handleLength);
+            //(tempHandleTable->handle)[handleLength] = '\0';             // NULL terminate
+            flag = 0;
+        }
+        index++;
+        tempHandleTable += 1;       // next node
+    }
+
 }
